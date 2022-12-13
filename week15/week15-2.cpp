50faf8e50413c0a53159e5b3071c10748760e93e
cjuss <cc4257951@gmail.com>
2022-12-13 12:02:53 +0800

    week15

---
+++ week15/week15-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+
+        int a[4]={1,2,3,4};
+        char b[4]={49,50,51,52};
+        printf("%d:%c\n",49,49);
+        printf("%d:%c\n",65,65);
+        printf("%d:%c\n",97,97);
+}
+
